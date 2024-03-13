b3115509cf95220a1b1d84656953bc1dc80c39a2
febift <febifian23@gmail.com>
2024-03-13 13:27:55 +0800

    Upload berkas proyek

---
+++ proyek-submission-mpk-2.0.2/src/main/kotlin/com/dicoding/exam/latihan3/App.kt
@@ -0,0 +1,57 @@
+/****************************************************************************************************
+ *                               Perhatian                                                          *
+ *                                                                                                  *
+ *          Agar dapat diperiksa dengan baik, hindari beberapa hal berikut:                         *
+ *                                                                                                  *
+ *          1. Mengubah kode yang berada di dalam fungsi main()                                     *
+ *          2. Mengubah atau menghapus kode yang sudah ada secara default                           *
+ *          3. Membuat fungsi baru yang bukan merupakan tugas latihan                               *
+ *          4. Mengubah struktur project (menghapus, mengedit, dan memindahkan package)             *
+ *                                                                                                  *
+ ***************************************************************************************************/
+
+package com.dicoding.exam.latihan3
+
+/**
+ *  TODO
+ *  Lengkapi fungsi di bawah ini agar dapat mengembalikkan tipe nilai sesuai dengan
+ *  parameter yang dilampirkan. Contohnya seperti berikut:
+ *
+ *  input: checkType(10)
+ *  output: "Yes! it's Integer"
+ *
+ *  Berikut adalah beberapa tipe nilai yang wajib ada.
+ *  • Integer
+ *  • String
+ *  • Boolean
+ *  • Double
+ *  • List<String>
+ *  • Map<String, String>
+ *
+ */
+fun <T> checkType(args: T): String {
+    val data = args
+    var dataType = ""
+
+    when(data) {
+        is String -> dataType = "String"
+        is Int -> dataType = "Integer"
+        is Boolean -> dataType = "Boolean"
+        is Double -> dataType = "Double"
+        is List<*> -> dataType = "List"
+        is Map<*,*> -> dataType = "Map"
+    }
+
+    return "Yes! it's ${dataType}"
+}
+
+fun main() {
+    println(
+        """
+        '[10, 9, 8 , 6]' is List? ${checkType(listOf(10, 9, 8, 6))}
+        'Dicoding Indonesia' is String? ${checkType("Dicoding Indonesia")}
+        'True' is Boolean? ${checkType(true)}
+        '10.01' is Double? ${checkType(10.01)}
+        """.trimIndent()
+    )
+}
